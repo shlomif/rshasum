ffa3e486085660b0556033bcd338040f4429f76b
Shlomi Fish <shlomif@shlomifish.org>
2018-04-25 15:33:00 +0300

    Add copyright.

--- rshasum.bash
+++ rshasum.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the ISC license.
+#
+
 export SUM="${RSHASUM_SUM:-sha256sum}"
 tfn="`mktemp`"
 find . -type f -follow | (LC_ALL=C sort) | xargs -d '\n' "$SUM" | tee "$tfn"
