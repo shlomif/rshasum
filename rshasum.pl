31842ca297b2b53db60aacebb19c2c22e62b26aa
Shlomi Fish <shlomif@shlomifish.org>
2019-02-24 13:32:53 +0200

    A more efficient perl version.

---
+++ rshasum.pl
@@ -0,0 +1,39 @@
+## Please see file perltidy.ERR
+#!/usr/bin/env perl
+#
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the terms of the ISC license.
+#
+#
+use strict;
+use warnings;
+
+use File::Find::Object ();
+use Digest             ();
+
+use Getopt::Long qw/ GetOptions /;
+
+my $digest;
+GetOptions( 'digest=s' => \$digest, )
+    or die "foo $!";
+
+my $t = Digest->new($digest);
+
+my $ff = File::Find::Object->new( {}, "." );
+while ( my $r = $ff->next_obj )
+{
+    if ( $r->is_file )
+    {
+        my $d    = Digest->new($digest);
+        my $path = $r->path;
+        open my $fh, '<', $path;
+        binmode $fh;
+        $d->addfile($fh);
+        my $s = $d->hexdigest . '  ' . $path . "\n";
+        print $s;
+        $t->add($s);
+    }
+}
+my $s = $t->hexdigest . '  ' . "-" . "\n";
+print $s;
--- tests/test-bash.t
+++ tests/test-bash.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 use Test::Differences (qw( eq_or_diff ));
 use IO::All qw/ io /;
 use Cwd qw/ getcwd /;
@@ -11,6 +11,19 @@ use Cwd qw/ getcwd /;
 {
     my $cwd = getcwd();
     system(qq%cd tests/data/1/ && "$cwd"/rshasum.bash > ../have1.txt%);
+
+    # TEST
+    eq_or_diff(
+        io->file("tests/data/have1.txt")->all,
+        io->file("tests/data/want1.txt")->all,
+        "rshasum #1",
+    );
+}
+{
+    my $cwd = getcwd();
+    system(
+qq%cd tests/data/1/ && $^X "$cwd"/rshasum.pl --digest SHA-256 > ../have1.txt%
+    );
 
     # TEST
     eq_or_diff(
