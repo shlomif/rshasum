706f6e552fd6417080f694679e17b64a31aa6030
Shlomi Fish <shlomif@shlomifish.org>
2019-03-09 20:39:09 +0200

    better error and docs

--- App-rshasum/lib/App/rshasum.pm
+++ App-rshasum/lib/App/rshasum.pm
@@ -13,6 +13,10 @@ sub run
     my $digest;
     GetOptions( 'digest=s' => \$digest, )
         or die "foo $!";
+    if ( not defined($digest) )
+    {
+        die "Please give a --digest=[digest] argument.";
+    }
 
     my $t = Digest->new($digest);
 
@@ -41,6 +45,10 @@ sub run
 =head1 NAME
 
 App::rshasum - recursive shasum.
+
+=head1 SYNOPSIS
+
+    rshasum --digest=SHA-256
 
 =head1 METHODS
 
