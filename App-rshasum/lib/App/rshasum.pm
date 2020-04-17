d9c730afd0a4788354f5b7e55998aaae1c70c20b
Shlomi Fish <shlomif@shlomifish.org>
2020-04-17 16:45:15 +0300

    add skip

--- App-rshasum/lib/App/rshasum.pm
+++ App-rshasum/lib/App/rshasum.pm
@@ -7,6 +7,7 @@ use File::Find::Object ();
 use Digest             ();
 
 use Getopt::Long qw/ GetOptions /;
+use List::Util 1.33 qw/ any /;
 
 sub _worker
 {
@@ -14,16 +15,26 @@ sub _worker
 
     my $digest    = $args->{digest};
     my $output_cb = $args->{output_cb};
+    my @prunes    = @{ $args->{prune_re} || [] };
 
     my $t = Digest->new($digest);
 
     my $ff = File::Find::Object->new( {}, "." );
+FILES:
     while ( my $r = $ff->next_obj )
     {
+        my $path = join '/', @{ $r->full_components };
+        if (@prunes)
+        {
+            if ( any { $path =~ $_ } @prunes )
+            {
+                $ff->prune;
+                next FILES;
+            }
+        }
         if ( $r->is_file )
         {
-            my $d    = Digest->new($digest);
-            my $path = join '/', @{ $r->full_components };
+            my $d = Digest->new($digest);
             open my $fh, '<', $r->path;
             binmode $fh;
             $d->addfile($fh);
@@ -42,7 +53,8 @@ sub _worker
 sub run
 {
     my $digest;
-    GetOptions( 'digest=s' => \$digest, )
+    my @skips;
+    GetOptions( 'digest=s' => \$digest, 'skip=s' => \@skips, )
         or die "foo $!";
     if ( not defined($digest) )
     {
@@ -51,7 +63,8 @@ sub run
     return shift()->_worker(
         {
             digest    => $digest,
-            output_cb => sub { print shift()->{str}; }
+            output_cb => sub { print shift()->{str}; },
+            prune_re  => ( \@skips ),
         }
     );
 }
@@ -65,6 +78,7 @@ App::rshasum - recursive shasum.
 =head1 SYNOPSIS
 
     rshasum --digest=SHA-256
+    rshasum --digest=SHA-256 --skip='\.sw[a-zA-Z]*\z' --skip='~\z'
 
 =head1 METHODS
 
